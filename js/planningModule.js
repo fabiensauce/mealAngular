ef923c3bb46a26d74402a061b830b4607a1fd793
fabien <fabiensauce@orange.fr>
2015-12-07 12:03:59 +0100

    last commit

--- js/planningModule.js
+++ js/planningModule.js
@@ -25,25 +25,24 @@
 		
 		$scope.columns = [{title:"1"}, {title:"2"}, {title:"3"}, {title:"4"}];
     
-    $rootScope.$on('dropEvent', function(evt, dragged, dropped) {
-        var i, oldIndex1, oldIndex2;
-        for(i=0; i<$scope.columns.length; i++) {
-            var c = $scope.columns[i];
-            if(dragged.title === c.title) {
-                oldIndex1 = i;
-            }
-            if(dropped.title === c.title) {
-                oldIndex2 = i;
-            }
-        }
-        var temp = $scope.columns[oldIndex1];
-        $scope.columns[oldIndex1] = $scope.columns[oldIndex2];
-        $scope.columns[oldIndex2] = temp;
-        $scope.$apply();
-    });
+		$rootScope.$on('dropEvent', function(evt, dragged, dropped) {
+			var i, oldIndex1, oldIndex2;
+			for(i=0; i<$scope.columns.length; i++) {
+				var c = $scope.columns[i];
+				if(dragged.title === c.title) {
+					oldIndex1 = i;
+				}
+				if(dropped.title === c.title) {
+					oldIndex2 = i;
+				}
+			}
+			var temp = $scope.columns[oldIndex1];
+			$scope.columns[oldIndex1] = $scope.columns[oldIndex2];
+			$scope.columns[oldIndex2] = temp;
+			$scope.$apply();
+		});
     }
 ]);
-
 
 
 
